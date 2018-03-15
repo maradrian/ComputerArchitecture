602a177862c1b67584585ee8d979d869b5c58ef4
patmos <karci.marso@gmail.com>
2018-03-15 19:39:28 +0100

    Author: Karol Marso Subject: First test Comments: I created first test class for our NIC. It issues command for writing the dataat specified address, awaits for one clock cycle, peeks for response, waits for one clock cycle and the isses command for reading the data, sleeps for 1 cycle and then peeks for the response again.

---
+++ TEST/OCP_TEST.scala
@@ -0,0 +1,50 @@
+/*
+ * Simple testbench supposed to pour some OCP signals to our NIC.
+ * This one tests writing the address, data and response and then also reading the data.
+ * ports master: MCmd, MAddr, MData, MByteEn
+ * ports slave: SData, SResp
+ */
+import Chisel._
+import ocp._
+import patmos.Constants._
+
+/*// Constants for MCmd
+object OcpCmd {
+  val IDLE = Bits("b000")
+  val WR   = Bits("b001")
+  val RD   = Bits("b010")
+//  val RDEX = Bits("b011")
+//  val RDL  = Bits("b100")
+//  val WRNP = Bits("b101")
+//  val WRC  = Bits("b110")
+//  val BCST = Bits("b111")
+}
+
+// Constants for SResp
+object OcpResp {
+  val NULL = Bits("b00")
+  val DVA  = Bits("b01")
+  val FAIL = Bits("b10")
+  val ERR  = Bits("b11")
+}*/
+
+class OCP_TEST(dut: NIC) extends Tester(dut) {
+  //issues command to write the data
+  poke(dut.io.ocp.M.Cmd, OcpCmd.WR)
+  poke(dut.io.ocp.M.Data, UInt(15))
+  poke(dut.io.ocp.M.Addr, "he8000003")
+  //sleeps for one clock cycle
+  step(1)
+  //peeks data valid response
+  peek(dut.io.ocp.S.Resp, OcpResp.DVA)
+  //sleeps one clock cycle
+  step(1)
+  //issues command to read the data
+  poke(dut.io.ocp.M.Cmd, OcpCmd.RD)
+  poke(dut.io.ocp.M.Addr, "he8000003")
+  //sleeps one clock cycle
+  step(1)
+  //peeks for data valid response and data
+  peek(dut.io.ocp.S.Data, UInt(15))
+  peek(dut.io.ocp.S.Resp, OcpResp.DVA)
+}
