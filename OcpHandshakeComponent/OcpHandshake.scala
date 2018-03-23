da6f3a6bb35bed96a6360353bd7ce8c5ff5b14f5
patmos <karci.marso@gmail.com>
2018-03-23 17:54:15 +0100

    Author:Karol Marso Subject: Prepared tests for the handshake component I prepared testing environment for the handshake component. It is now only needed to run it in sbt as runMain NOC.OcpHandshakeComponent.HandshakeTest

--- OcpHandshakeComponent/OcpHandshake.scala
+++ OcpHandshakeComponent/OcpHandshake.scala
@@ -7,6 +7,89 @@ import patmos.Constants._
 import ocp._
 
 
-/*class Handshake() extends CoreDevice() {
+class Handshake() extends Module {
+   val io = new Bundle{
+      val fromCore = new OcpCoreSlavePort(ADDR_WIDTH, DATA_WIDTH)//data goes from processor
+      val toTX = new OcpCoreMasterPort(ADDR_WIDTH, DATA_WIDTH)//data goes to TX
+      val TXReady = Input(Bool())//handshake needs to wait until TX component is ready
+      val TXValid = Output(Bool())//handshake has valid data
+   }
 
-}*/
+   
+}
+
+class HandshakeTest(dut: Handshake) extends Tester(dut){
+   /*
+    * first simple test when processor issues write command
+    * we issue WR Command and then peek in the tx to see the things
+    * SResp and SData is automatically directed to the processor
+    */
+   poke(dut.io.fromCore.M.Cmd, 0x0)//Null command
+   poke(dut.io.TXReady, true)
+   step(1)
+   poke(dut.io.fromCore.M.Cmd, 0x1)//Write command
+   poke(dut.io.fromCore.M.Addr, 0xe8000001)
+   poke(dut.io.fromCore.M.Data, 0xdeadbeef)
+   poke(dut.io.TXReady, true)
+   peek(dut.io.TXValid)
+   step(1)
+   poke(dut.io.fromCore.M.Cmd, 0x0)//Write command
+   poke(dut.io.TXReady, true)
+   peek(dut.io.fromCore.S.Resp)
+   peek(dut.io.TXValid)
+   step(1)
+   /*
+    * In this part we let the handshake component wait for a few steps
+    *
+    */
+   poke(dut.io.fromCore.M.Cmd, 0x0)//Null command
+   poke(dut.io.TXReady, true)
+   peek(dut.io.TXValid)
+   step(1)
+   poke(dut.io.fromCore.M.Cmd, 0x1)//Write command
+   poke(dut.io.fromCore.M.Addr, 0xe8000001)
+   poke(dut.io.fromCore.M.Data, 0xdeadbeef)
+   poke(dut.io.TXReady, false)
+   peek(dut.io.TXValid)
+   step(1)
+   poke(dut.io.fromCore.M.Cmd, 0x0)//Write command
+   poke(dut.io.TXReady, false)
+   peek(dut.io.fromCore.S.Resp)
+   peek(dut.io.TXValid)
+   step(1)
+   poke(dut.io.fromCore.M.Cmd, 0x0)//Write command
+   poke(dut.io.TXReady, false)
+   peek(dut.io.fromCore.S.Resp)
+   peek(dut.io.TXValid)
+   step(1)
+   poke(dut.io.fromCore.M.Cmd, 0x0)//Write command
+   poke(dut.io.TXReady, false)
+   peek(dut.io.fromCore.S.Resp)
+   peek(dut.io.TXValid)
+   step(1)
+   poke(dut.io.fromCore.M.Cmd, 0x0)//Write command
+   poke(dut.io.TXReady, true)
+   peek(dut.io.fromCore.S.Resp)
+   peek(dut.io.TXValid)
+   step(1)
+   poke(dut.io.fromCore.M.Cmd, 0x0)//Write command
+   poke(dut.io.TXReady, true)
+   peek(dut.io.fromCore.S.Resp)
+   peek(dut.io.TXValid)
+   step(1)
+   /*
+    * The last part of the test tests read commmand evn though we probably won`t use it
+    *
+    *
+    */
+} 
+
+object HandshakeTest{
+  def main(args: Array[String]): Unit = {
+    chiselMainTest(Array("--genHarness", "--test", "--backend", "c",
+      "--compile", "--vcd", "--targetDir", "generated"),
+      () => Module(new Handshake())) {
+        m => new HandshakeTest(m)
+      }
+  }
+}
