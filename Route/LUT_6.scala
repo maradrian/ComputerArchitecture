ff2fcd945b664ed210f567de7afa25258c9e047a
stookit <leave.me@alone.please>
2018-04-03 16:49:08 +0200

    LUTs + LUT generator

---
+++ Route/LUT_6.scala
@@ -0,0 +1,28 @@
+package NOC.Route
+
+import Chisel._
+
+class LUT_6() extends Module(){
+	val io = IO(new Bundle{
+		val dst_addr = Input(UInt(width = 4))
+		val route_out = Output(UInt(width = 10))
+	})
+
+	when(io.dst_addr === UInt(0)){
+		io.route_out := UInt(b"0111000000")
+	}.elsewhen(io.dst_addr === UInt(1)){
+		io.route_out := UInt(b"1001110000")
+	}.elsewhen(io.dst_addr === UInt(2)){
+		io.route_out := UInt(b"1010011100")
+	}.elsewhen(io.dst_addr === UInt(3)){
+		io.route_out := UInt(b"0101110000")
+	}.elsewhen(io.dst_addr === UInt(4)){
+		io.route_out := UInt(b"1001011100")
+	}.elsewhen(io.dst_addr === UInt(5)){
+		io.route_out := UInt(b"1010010111")
+	}.elsewhen(io.dst_addr === UInt(7)){
+		io.route_out := UInt(b"1011000000")
+	}.elsewhen(io.dst_addr === UInt(8)){
+		io.route_out := UInt(b"1010110000")
+	}
+}
