a20816902007b168d5dddf0155b6108024e37507
patmos <karci.marso@gmail.com>
2018-04-03 15:20:12 +0200

    Author: Karol Marso Subject: Incomplete test

--- NIC_TEST/NIC_TEST.scala
+++ NIC_TEST/NIC_TEST.scala
@@ -7,4 +7,44 @@ import patmos.Constants._
 import io._
 import ocp._
 
+class NicTest(dut: top) extends Tester(dut){
+  //first issue few commands from Processor to write to NIC
+  for (i <- 0 to 5){
+    poke(io.fromProcessor.M.Cmd, 0x1)
+    poke(io.fromProcessor.M.Addr, 0xe801ffff)
+    poke(io.fromProcessor.M.Data, i)
+    poke(io.routerReadyOut, false)
+    step(1)
+    poke(io.routerReadyOut, false)
+    peek(io.fromProcessor.S.Resp)
+    step(1)
+    poke(io.routerReadyOut, false)
+    peek(io.fromProcessor.S.Resp)
+    step(1)
+    poke(io.routerReadyOut, true)
+    peek(io.fromProcessor.S.Resp)
+    step(1)
+    peek(io.fromProcessor.S.Resp)
+    step(1)
+    peek(io.fromProcessor.S.Resp)
+    step(1)
+    peek(io.fromProcessor.S.Resp)
+    step(1)
+    peek(io.fromProcessor.S.Resp)
+    step(1)
+    peek(io.fromProcessor.S.Resp)
+    step(1)
+    peek(io.fromProcessor.S.Resp)
+    step(1)
+    peek(io.fromProcessor.S.Resp)
+  }
 
+  //now mix it a bit - send few commands to write to NIC and a few commands to local memory
+  for (i <- 0 to 10){
+    val addr = 0xe8000000
+    val proc = i%
+  }
+  
+  //now read the data from the memory since we should have something in there
+  
+}
