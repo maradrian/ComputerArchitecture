549021ac850a21e700a711dd578e169fd33a1b96
stookit <leave.me@alone.please>
2018-04-07 13:52:50 +0200

    NOC blackbox update

--- NOC_chisel/NOC_chisel.scala
+++ NOC_chisel/NOC_chisel.scala
@@ -1,16 +1,16 @@
 package NOC_chisel
 
 
-class NOC_chisel(packet_width: Int = 96) extends Blackbox{
+class NOC_chisel(packet_width: Int = 96, no_of_routers: Int) extends Blackbox{
 	val io = new Bundle() {
 			//INPUT
-			val local_packet_in = Vec(9, BITS(OUTPUT, width = packet_width))
-			val local_valid_in = Vec(9, BITS(OUTPUT, width = 1))
-			val local_ready_out = Vec(9, BITS(INPUT, width = 1))
+			val local_packet_in = Vec(no_of_routers, BITS(OUTPUT, width = packet_width))
+			val local_valid_in = Vec(no_of_routers, BITS(OUTPUT, width = 1))
+			val local_ready_out = Vec(no_of_routers, BITS(INPUT, width = 1))
 			//OUTPUT
-			val local_packet_out = Vec(9, BITS(INPUT, width = packet_width))
-			val local_valid_out = Vec (9, BITS(INPUT, width = 1))
-			val local_ready_in = Vec(9, BITS(OUTPUT, width = 1))
+			val local_packet_out = Vec(no_of_routers, BITS(INPUT, width = packet_width))
+			val local_valid_out = Vec(no_of_routers, BITS(INPUT, width = 1))
+			val local_ready_in = Vec(no_of_routers, BITS(OUTPUT, width = 1))
 
 		} with NOC_chisel.Pins
 
@@ -19,10 +19,10 @@ class NOC_chisel(packet_width: Int = 96) extends Blackbox{
 	renameClock(clock, clk)
 	reset.setName("rst")
 
-	var i = 0;
+	var i = 0
 
-	
-	for( a <- 0 to 8){
+
+	for( a <- 0 until no_of_routers){
 		//INPUT
 		io.local_packet_in(i).setName(f"local_packet_in_$i")
 		io.local_valid_in(i).setName(f"local_valid_in_$i")
