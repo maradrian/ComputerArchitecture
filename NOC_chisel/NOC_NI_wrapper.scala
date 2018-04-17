af6a4c43d1467244ba5886299dee8539c3618bb3
Unknown <zuzanajelcicova@gmail.com>
2018-04-17 13:58:50 +0200

    Temporary Wrapper

--- NOC_chisel/NOC_NI_wrapper.scala
+++ NOC_chisel/NOC_NI_wrapper.scala
@@ -11,24 +11,26 @@ class OCPPorts extends Bundle{
    val fromProcessor = new OcpCoreSlavePort(ADDR_WIDTH, DATA_WIDTH)
 }
 
-class Wrapper(nrCores : Int) extends BlackBox(){
-   val io = Vec(nrCores, new OCPPorts())
-   
+class Wrapper(nrCores : Int) extends Module(){
+   val io = new Bundle{
+	val io = Vec(nrCores, new OCPPorts())
+ }
+
    val NoC  = Module(new NOC_chisel(96, nrCores))
    val NIs = Module(new TopGenerator(nrCores))
    
    for (i <- 0 until nrCores){
       //connect to network
-      NoC.io.local_packet_out(i) := NIs.io(i).routerPacketIn
-      NoC.io.local_valid_out(i) := NIs.io(i).routerValidIn
-      NoC.io.local_ready_out(i) := NIs.io(i).routerReadyIn
+      NoC.io.local_packet_out(i) <> NIs.io(i).routerPacketIn
+      NoC.io.local_valid_out(i) <> NIs.io(i).routerValidIn
+      NoC.io.local_ready_out(i) <> NIs.io(i).routerReadyIn
       //connect from network
-      NIs.io(i).routerPacketOut := NoC.io.local_packet_in(i)
-      NIs.io(i).routerReadyOut := NoC.io.local_ready_in(i)
-      NIs.io(i).routerValidOut := NoC.io.local_valid_in(i)
+      NIs.io(i).routerPacketOut <> NoC.io.local_packet_in(i)
+      NIs.io(i).routerReadyOut <> NoC.io.local_ready_in(i)
+      NIs.io(i).routerValidOut <> NoC.io.local_valid_in(i)
       //connection with patmos
-      NIs.io(i).fromProcessor.M <> io(i).fromProcessor.M
-      NIs.io(i).fromProcessor.S <> io(i).fromProcessor.S
+      NIs.io(i).fromProcessor.M <> io.io(i).fromProcessor.M
+      NIs.io(i).fromProcessor.S <> io.io(i).fromProcessor.S
    }
 }
 
@@ -37,8 +39,10 @@ object Wrapper {
     chiselMain(Array("--backend", "v"), () => Module(new Wrapper(9)))
   }
 }
+
+
 class WrapperTester(dut: Wrapper) extends Tester(dut){
-   poke(dut.io(0).fromProcessor.M.Cmd, 0x01)
+   //poke(dut.io(0).fromProcessor.M.Cmd, 0x01)
 }
 
 object WrapperTester {
