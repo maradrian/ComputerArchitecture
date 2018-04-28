f1359799f4963c18dc79a4479fea313d926c8963
Victor <mardari.adri@gmail.com>
2018-04-28 05:18:31 +0200

    Ughht

--- Memory/Memory.scala
+++ Memory/Memory.scala
@@ -1,7 +1,7 @@
 package NOC.Memory
- 
+
 import Chisel._
- 
+
 /*
  * Dual port memory
  * Port 1 is Read/Write
@@ -26,14 +26,14 @@ class Memory() extends Module{
     when(io.enable2 === Bool(true)){
       syncMem(io.addr2) := io.dataIn2
     }*/
- 
+
   // read
   //val rdAddrReg  = Reg(next = io.addr)
   val rdAddrReg2 = Reg(next = io.addr2)
   //io.dataOut  := syncMem(rdAddrReg)
   io.dataOut2 := syncMem(rdAddrReg2)
 }
- 
+
 class MemoryTest(dut: Memory) extends Tester(dut){
   //first write some data
   poke(dut.io.enable, true)
@@ -49,7 +49,61 @@ class MemoryTest(dut: Memory) extends Tester(dut){
   poke(dut.io.addr, 0xfffe)
   step(1)
   poke(dut.io.enable2, false)  
- 
+
   //now let`s read them
-commit:3dbd3c
-Memory read update
+  poke(dut.io.enable, false)
+  poke(dut.io.addr, 0x000f)
+  poke(dut.io.addr2, 0xfffe)
+  step(1)
+  poke(dut.io.enable, false)
+  poke(dut.io.addr, 0x0003)
+  step(1)
+  poke(dut.io.enable, false)
+  poke(dut.io.addr, 0xfffe)
+  poke(dut.io.addr2, 0x000f)
+  step(1)
+
+   //now write some data to the second port
+  poke(dut.io.enable2, true)
+  poke(dut.io.dataIn2, 0xdeadbeef)
+  poke(dut.io.addr2, 0x00ff)
+  step(1)
+  poke(dut.io.enable2, true)
+  poke(dut.io.dataIn2, 0x0000123a)
+  poke(dut.io.addr2, 0x0013)
+  step(1)
+  poke(dut.io.enable2, true)
+  poke(dut.io.dataIn2, 0x20002000)
+  poke(dut.io.addr2, 0xffff)
+  step(1)
+  
+  //now let`s read them
+  poke(dut.io.enable2, false)
+  poke(dut.io.addr, 0x000f)
+  poke(dut.io.addr2, 0xffff)
+  step(1)
+  poke(dut.io.enable2, false)
+  poke(dut.io.addr2, 0x0013)
+  peek(dut.io.dataOut2)
+  peek(dut.io.dataOut)
+  step(1)
+  poke(dut.io.enable2, false)
+  poke(dut.io.addr, 0x00ff)
+  poke(dut.io.addr2, 0x00ff)
+  peek(dut.io.dataOut2)
+  step(1)
+  peek(dut.io.dataOut)
+  peek(dut.io.dataOut2)
+  step(1)
+  step(1)
+}
+
+object MemoryTest{
+  def main(args: Array[String]): Unit = {
+    chiselMainTest(Array("--genHarness", "--test", "--backend", "c",
+      "--compile", "--vcd", "--targetDir", "generated"),
+      () => Module(new Memory())) {
+        m => new MemoryTest(m)
+      }
+  }
+}
